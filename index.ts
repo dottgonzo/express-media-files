f17d8f566ad60c42ce0610fd5789537b6d89dac5
Dario Caruso <dev@dariocaruso.info>
2017-07-28 00:18:22 +0200

    todo

--- index.ts
+++ index.ts
@@ -5,42 +5,64 @@ import * as bodyParser from 'body-parser'
 import * as  jwt from 'jsonwebtoken'
 
 
-export default function(path,config?:{mode?:{type:string,secret?:string}}){
+export default function (path, config?: { mode?: { type: string, secret?: string } }) {
 
 
-const router = express.Router();
+    const router = express.Router();
 
-const fflist = new mediawatch.mediafiles({ path: path })
-
-
-router.use(bodyParser.json())
-router.use(bodyParser.urlencoded({ extended: true }))
+    const fflist = new mediawatch.mediafiles({ path: path })
 
 
-// use res.render to load up an ejs view file
+    router.use(bodyParser.json())
+    router.use(bodyParser.urlencoded({ extended: true }))
 
 
-function getFilesByToken(token){
-return new Promise((resolve,reject)=>{
+    // use res.render to load up an ejs view file
 
 
-
-
-})
-
-}
+    function getFilesByToken(token) {
+        return new Promise((resolve, reject) => {
 
 
 
-router.get('/user/:token/list', function (req, res) {
-    res.json({ list: fflist.list })
-})
-router.get('/user/:token/listjs', function (req, res) {
-    res.send('var list='+JSON.stringify(fflist.list))
-})
+
+        })
+
+    }
+    router.get('/list', function (req, res) {
+        if (!config || !config.mode || config.mode.type !== 'restricted') {
+            res.json({ list: fflist.list })
+        } else {
+            res.json({ error: 'rootmode not allowed' })
+        }
+    })
+    router.get('/listjs', function (req, res) {
+        if (!config || !config.mode || config.mode.type !== 'restricted') {
+            res.send('var list=' + JSON.stringify(fflist.list))
+        } else {
+            res.send("alert('user mode not allowed')")
+        }
+    })
 
 
-return router
+
+    router.get('/user/:token/list', function (req, res) {
+
+        if(){
+
+            res.json({ list: fflist.list })
+
+        } else {
+            res.json({error:'json not valid'})
+        }
+
+    })
+    router.get('/user/:token/listjs', function (req, res) {
+        res.send('var list=' + JSON.stringify(fflist.list))
+    })
+
+
+    return router
 
 
 
